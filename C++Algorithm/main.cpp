233b6b14734b27de54a85047123f584f6747ab93
Tetsu Lee <tlee@jfrontier.net>
2023-05-18 01:33:57 +0900

    コメント修正

--- C++Algorithm/main.cpp
+++ C++Algorithm/main.cpp
@@ -3,15 +3,14 @@
 int main() {
 	
 	Algorithm algorithm = Algorithm();
-	//配列作成
-	int sort[5] = { 0,4,2,3,1 }; //配列
+	//テスト用配列作成
+	int sort[5] = { 0,4,2,3,1 };
 
-	//クイックソート
 	algorithm.selectionSort(sort, 5);
 
 	for (int i = 0; i < 5; i++) {
 
-		std::cout << sort[i] << ' '; //標準出力
+		std::cout << sort[i] << ' ';
 	}
 
 	std::cout << std::endl;
